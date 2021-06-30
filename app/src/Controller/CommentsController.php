ed171e4a7a5c1d90e523c265180c4817c9a4b61f
FmarshalD <kirh09764@gmail.com>
2021-06-30 10:58:18 +0700

    comment creation added

---
+++ app/src/Controller/CommentsController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Comment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentsController extends AbstractController
+{
+    /**
+     * @Route("/comments", name="comments")
+     */
+    public function index(Request $request): Response
+    {
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CommentsController.php',
+        ]);
+    }
+
+    public function store(Request $request,ValidatorInterface $validator):Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $comment = new Comment();
+        $comment->setText($request->text);
+        $comment->setDate(date('m/d/Y h:i:s a', time()));
+        $comment->setUserid($request->user_id);
+
+        $errors = $validator->validate($comment);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+        else{
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return new Response($this->json([
+                'id'=> $comment->getId(),
+                'text' => $comment->getText(),
+                'date' => $comment->getDate(),
+                'user_id'=>$comment->getUserid()
+            ]),200);
+        }
+
+    }
+
+    public function update(Request $request):Response
+    {
+        $this->validate($request, [
+            'text' => 'required|max:1000'
+        ]);
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CommentsController.php',
+        ]);
+    }
+
+    public function destroy(Request $request):Response
+    {
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CommentsController.php',
+        ]);
+    }
+}
---
+++ app/src/Entity/Comment.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUserid(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserid(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
